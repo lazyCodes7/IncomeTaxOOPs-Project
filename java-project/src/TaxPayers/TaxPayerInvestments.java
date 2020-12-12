bb53bcb2ab949c5a65c87c58773174cbf10175ec
lazyCodes7 <rishabmudliar@gmail.com>
2020-12-12 13:12:25 +0530

    Added client-side validation to investments and taxslabs

--- java-project/src/TaxPayers/TaxPayerInvestments.java
+++ java-project/src/TaxPayers/TaxPayerInvestments.java
@@ -1,10 +1,13 @@
 package TaxPayers;
 
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -15,60 +18,79 @@ public class TaxPayerInvestments {
 	private double ProvidedFund;
 	private double Policies;
 	public static void main(String args[]) {
+		new TaxPayerInvestmentsLayout();
+		
+	}
+	static class TaxPayerInvestmentsLayout {
+		ActionListener saveCredentials  = new ActionListener() {
+    		public void actionPerformed(ActionEvent e) {
+    			JOptionPane.showMessageDialog(frame, "Credentials Saved");
+
+            }
+    		
+    	};
+    	
 		JFrame frame = new JFrame();
-		frame.setTitle("Income Tax Management Corp");
 		JPanel panel = new JPanel();
-		frame.add(panel);
-		panel.setLayout(null);
-		frame.setAlwaysOnTop(true);
 		JLabel l = new JLabel("Your Investments");
-		l.setBounds(300, 10, 400, 75);
 		Font labelFont = l.getFont();
 		String labelText = l.getText();
 		JLabel housingLoans = new JLabel("Housing Loan");
-		housingLoans.setBounds(200,120,150,25);
-		
 		JLabel otherLoans = new JLabel("Other Loans");
-		otherLoans.setBounds(200,180,80,25);
 		JLabel rent = new JLabel("Rent");
-		rent.setBounds(200,240,200,25);
 		JLabel providedFunds = new JLabel("Provided Funds");
-		providedFunds.setBounds(200,300,200,25);
 		JLabel policies = new JLabel("Policies?");
-		policies.setBounds(200,360,200,25);
 		JTextField housingLoansTF = new JTextField(20);
-		housingLoansTF.setBounds(330, 120, 165, 25);
 		JTextField otherLoansTF = new JTextField(20);
-		otherLoansTF.setBounds(330, 180, 165, 25);
 		JTextField rentTF = new JTextField(20);
-		rentTF.setBounds(330, 240, 165, 25);
 		JTextField providedFundsTF = new JTextField(20);
-		providedFundsTF.setBounds(330, 300, 165, 25);
 		JTextField policiesTF = new JTextField(20);
-		policiesTF.setBounds(330,360,165,25);
 		JButton button1 = new JButton("Save Credentials");
-		button1.setBounds(50,420,200,25);
-		
 		JButton button2 = new JButton("Edit Credentials");
-		button2.setBounds(280, 420, 150, 25);
-		JButton button3 = new JButton("Go Back");
-		button3.setBounds(450, 420, 200, 25);
-		
-		panel.add(l);
-		panel.add(housingLoans);
-		panel.add(otherLoans);
-		panel.add(rent);
-		panel.add(providedFunds);
-		panel.add(policies);
-		panel.add(housingLoansTF);
-		panel.add(otherLoansTF);
-		panel.add(rentTF);
-		panel.add(providedFundsTF);
-		panel.add(policiesTF);
-		panel.add(button1);
-		panel.add(button2);
-		panel.add(button3);
-		frame.setVisible(true);
-		frame.setSize(700,500);
+		TaxPayerInvestmentsLayout() {
+			frame.setTitle("Income Tax Management Corp");
+			frame.add(panel);
+			panel.setLayout(null);
+			frame.setAlwaysOnTop(true);
+			l.setBounds(300, 10, 400, 75);
+			
+			housingLoans.setBounds(200,120,150,25);
+			
+			otherLoans.setBounds(200,180,80,25);
+			rent.setBounds(200,240,200,25);
+			providedFunds.setBounds(200,300,200,25);
+			policies.setBounds(200,360,200,25);
+			housingLoansTF.setBounds(330, 120, 165, 25);
+			otherLoansTF.setBounds(330, 180, 165, 25);
+			rentTF.setBounds(330, 240, 165, 25);
+			providedFundsTF.setBounds(330, 300, 165, 25);
+			policiesTF.setBounds(330,360,165,25);
+			button1.setBounds(50,420,200,25);
+			
+			button2.setBounds(280, 420, 150, 25);
+			JButton button3 = new JButton("Go Back");
+			button3.setBounds(450, 420, 200, 25);
+			
+			panel.add(l);
+			panel.add(housingLoans);
+			panel.add(otherLoans);
+			panel.add(rent);
+			panel.add(providedFunds);
+			panel.add(policies);
+			panel.add(housingLoansTF);
+			panel.add(otherLoansTF);
+			panel.add(rentTF);
+			panel.add(providedFundsTF);
+			panel.add(policiesTF);
+			panel.add(button1);
+			panel.add(button2);
+			panel.add(button3);
+			button1.addActionListener(saveCredentials);
+			frame.setVisible(true);
+			frame.setSize(700,500);
+		}
+
+
 	}
+	
 }
--- java-project/src/TaxSlab/CalTaxSlab.java
+++ java-project/src/TaxSlab/CalTaxSlab.java
@@ -1,5 +1,86 @@
 package TaxSlab;
 
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 public class CalTaxSlab {
-	
+	public static void main(String args[]) {
+		new CalTaxSlabLayout();
+	}
+	static class CalTaxSlabLayout{
+		ActionListener checkTaxSlabAmt  = new ActionListener() {
+    		public void actionPerformed(ActionEvent e) {
+    			JOptionPane.showMessageDialog(frame, "Tax Slab is "+"153435 RS");
+            }
+    		
+    	};
+    	ActionListener goBack = new ActionListener() {
+    		public void actionPerformed(ActionEvent e) {
+    			JOptionPane.showMessageDialog(frame, "Success");
+    		}
+    	};
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JLabel l = new JLabel("Tax Slab Details");
+		Font labelFont = l.getFont();
+		String labelText = l.getText();
+		JLabel TotalIncome = new JLabel("Total Income");
+		JLabel TotalInvestment = new JLabel("Investments");
+		JLabel rateApplied = new JLabel("Rate");
+		JLabel Slab = new JLabel("Tax Slab");
+		
+		JTextField totalIncomeTF = new JTextField(20);
+		JTextField totalInvestmentTF = new JTextField(20);
+		JTextField rateAppliedTF = new JTextField(20);
+		JTextField slabTF = new JTextField(20);
+		JButton button1 = new JButton("Check Tax Slab Amount");
+		JButton button2 = new JButton("Go back");
+		CalTaxSlabLayout(){
+			frame.setTitle("Income Tax Management Corp");
+			frame.add(panel);
+			panel.setLayout(null);
+			frame.setAlwaysOnTop(true);
+			l.setBounds(320, 10, 150, 75);
+			TotalIncome.setBounds(200,120,80,25);
+			
+			TotalInvestment.setBounds(200,180,80,25);
+			
+			rateApplied.setBounds(200, 240, 165, 25);
+			
+			Slab.setBounds(200, 300, 165, 25);
+			totalIncomeTF.setBounds(300, 120, 165, 25);
+			totalInvestmentTF.setBounds(300, 180, 165, 25);
+			rateAppliedTF.setBounds(300, 240, 165, 25);
+			slabTF.setBounds(300, 300, 165, 25);
+
+			
+			button1.setBounds(150,350,200,25);
+			
+			button2.setBounds(450, 350, 150, 25);
+			
+			panel.add(l);
+			panel.add(TotalIncome);
+			panel.add(TotalInvestment);
+			panel.add(rateApplied);
+			panel.add(Slab);
+			panel.add(totalIncomeTF);
+			panel.add(rateAppliedTF);
+			panel.add(totalInvestmentTF);
+			panel.add(slabTF);
+			panel.add(button1);
+			panel.add(button2);
+			button2.addActionListener(goBack);
+			button1.addActionListener(checkTaxSlabAmt);
+			frame.setVisible(true);
+			frame.setSize(700,500);
+		}
+	}
 }
